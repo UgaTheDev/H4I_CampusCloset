32730bf6c0a133485902704365cb056f71cd2c0e
AndresL230 <190146319+AndresL230@users.noreply.github.com>
2026-04-10 03:20:49 -0400

    Build admin Donation Bin Manager with Leaflet pin-dropper and CRUD modals

---
+++ src/app/admin/bins/BinMap.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { useEffect } from 'react'
+import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+// Fix default marker icons
+delete (L.Icon.Default.prototype as Record<string, unknown>)._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+})
+
+const BU_CENTER: [number, number] = [42.3505, -71.1054]
+
+interface ClickHandlerProps {
+  onMapClick: (lat: number, lng: number) => void
+}
+
+function ClickHandler({ onMapClick }: ClickHandlerProps) {
+  useMapEvents({
+    click(e) {
+      onMapClick(e.latlng.lat, e.latlng.lng)
+    },
+  })
+  return null
+}
+
+interface BinMapProps {
+  lat: number | null
+  lng: number | null
+  onMapClick: (lat: number, lng: number) => void
+}
+
+export default function BinMap({ lat, lng, onMapClick }: BinMapProps) {
+  // Re-invalidate size when modal opens so tiles render correctly
+  useEffect(() => {
+    window.dispatchEvent(new Event('resize'))
+  }, [])
+
+  return (
+    <MapContainer
+      center={BU_CENTER}
+      zoom={15}
+      className="h-full w-full cursor-crosshair"
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://carto.com/">CartoDB</a>'
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+      />
+      <ClickHandler onMapClick={onMapClick} />
+      {lat !== null && lng !== null && (
+        <Marker position={[lat, lng]} draggable={false} />
+      )}
+    </MapContainer>
+  )
+}
